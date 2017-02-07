dea8fd696662503e3e872f803c4733f1c3967759
Matthew Smith <mcmullensmith@gmail.com>
2017-02-07 14:30:32 -0500

    scripts for score display and score keeping

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour {
+
+	public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (gameObject);
+	}
+
+	public void IncrementScore(int amount) {
+		
+		score += amount;
+		print("Your score: " + score);
+		// AudioSource source = GetComponent<AudioSource> ();
+		// source.Play();
+	}
+}
