4b625877add341692f0fbbe146a2ef5fcda05a1f
Matthew Smith <mcmullensmith@gmail.com>
2017-02-08 16:29:38 -0500

    added a level display to the UI

---
+++ Assets/Scripts/LevelDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelDisplay : MonoBehaviour {
+
+	Text text;
+
+	MarshMallowEmitter marshmallowEmitter;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+		marshmallowEmitter = FindObjectOfType<MarshMallowEmitter>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "Level " + marshmallowEmitter.level;
+	}
+}
