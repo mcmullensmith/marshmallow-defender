eee1665295e78216301d41837a6de3ab8b6b9823
Matthew Smith <mcmullensmith@gmail.com>
2017-02-08 12:43:00 -0500

    updated file to essesntially make it the game controller

--- Assets/Scripts/MarshMallowEmitter.cs
+++ Assets/Scripts/MarshMallowEmitter.cs
@@ -1,9 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class MarshMallowEmitter : MonoBehaviour {
 
 	public GameObject[] marshmallows;
+
+	//total collisions of marshmallows whether projectile, ground hit or destroyed outside mug
+	public int marshmallowHits = 0;
+
+	private int level = 1;
+
+	//max marshmallows per level
+	private int maxMarshmallows;
+	//max loops per level
+	private int maxLoops = 5;
 
 	public float horizontalArea = 10.0f;
 
@@ -11,25 +22,84 @@ public class MarshMallowEmitter : MonoBehaviour {
 
 	private float spawnTimer;
 
+	private bool isGameOver = false;
+
+	DamageKeeper damageKeeper;
+
 	// Use this for initialization
 	void Start () {
+		maxMarshmallows = marshmallows.Length * maxLoops;
 		spawnTimer = spawnDuration;
+		damageKeeper = FindObjectOfType<DamageKeeper>();
+		// StartCoroutine(LevelLoader());
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		print("marshmallow hits: " + marshmallowHits);
+		print("maxmarshmallows : " + maxMarshmallows);
+		print("Level: " + level);
+		print("Max : " + maxLoops);
+
+		if (damageKeeper.damage == 0) {
+			isGameOver = true;
+			print("Game Over");
+			SceneManager.LoadScene("GameOver");
+		}
+
+		
+		GenerateMarshmallows();
+
+	}
+
+	private IEnumerator LevelLoader() {
+
+		// while (marshmallowHits <  maxMarshmallows ) {
+			for (int i = 0; i< marshmallows.Length; i++) {
+				SpawnMarshmallows();
+				yield return new WaitForSeconds(spawnDuration);
+			}
+			
+		// }
+		
+		yield return new WaitForSeconds (5f);
+
+
+		if (marshmallowHits == maxMarshmallows && isGameOver == false) {
+			maxMarshmallows += 2;
+		 	level++;
+		 	marshmallowHits = 0;
+
+			StartCoroutine (LevelLoader ());
+		}
+	}
+
+	void GenerateMarshmallows() {
+		if ( marshmallowHits == maxMarshmallows && isGameOver == false ) {
+			print("Level over");
+			return;
+		}
+		
 		spawnTimer -= Time.deltaTime;
-		if (spawnTimer <= 0f) {
-			spawnTimer = spawnDuration;
-			for (int i = 0; i < marshmallows.Length; i++) {
-				GameObject marshmallow = Instantiate (marshmallows [Random.Range (0, marshmallows.Length)]);
+		if(maxLoops > 0) {
+			if ( spawnTimer <= 0f ) {
+				spawnTimer = spawnDuration;
+				for ( int i = 0; i< marshmallows.Length; i++ ) {
+					SpawnMarshmallows();
+				}
+				maxLoops--;
+			}
+		}
+		
+	}
+
+	void SpawnMarshmallows() {
+
+		GameObject marshmallow = Instantiate (marshmallows [Random.Range (0, marshmallows.Length)]);
 				marshmallow.transform.position = new Vector3 (
 					Random.Range (-horizontalArea, horizontalArea),
 					100.0f,
 					Random.Range (-horizontalArea, horizontalArea)
 				);
-			}
-		}
-
 	}
 }
