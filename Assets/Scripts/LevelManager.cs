62cb429de1744d7a5c245714c9e5f1d100d7fa54
Matthew Smith <mcmullensmith@gmail.com>
2017-02-09 19:30:40 -0500

    scripts for level manager and hud rotation

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void Quit() {
+		print("Quit application");
+		Application.Quit();
+	}
+
+	public void LoadScene(string name) {
+		print("Load scene by name");
+		SceneManager.LoadScene(name);
+	}
+
+	public void LoadNextScene() {
+		print("Load next scene");
+		int currentIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(currentIndex + 1);
+	}
+
+	public void LoadPrevScene() {
+		print("Load previous scene");
+		int currentIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(currentIndex - 1);
+	}
+}
