5e18c62a6567873ace5b4cb9701ca501b05e8c6f
Matthew Smith <mcmullensmith@gmail.com>
2017-02-11 14:24:40 -0500

    updated script to use vars

--- Assets/Scripts/HUDRotation.cs
+++ Assets/Scripts/HUDRotation.cs
@@ -11,8 +11,8 @@ public class HUDRotation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Camera camera = transform.parent.GetComponentInChildren<Camera>();
-		var projectedLookDirection = Vector3.ProjectOnPlane(camera.transform.forward, Vector3.up);
-		transform.rotation = Quaternion.LookRotation(projectedLookDirection);
+		var camera = transform.parent.GetComponent<Camera>();
+        var projectedLookDirection = Vector3.ProjectOnPlane(camera.transform.forward, Vector3.up);
+        transform.rotation = Quaternion.LookRotation(projectedLookDirection);
 	}
 }
