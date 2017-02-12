f4046574284a911d8209602613e34ca45a8136c9
Matthew Smith <mcmullensmith@gmail.com>
2017-02-12 09:32:06 -0500

    created a timer script

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public float timer = 10;
+	public Text timerText;
+
+
+	// Use this for initialization
+	void Start () {
+		timerText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		timerText.text = timer.ToString("f0");
+	}
+}
